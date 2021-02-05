38425483a34fd4176dc399812caa78c4d88b5637
jz8_armohb <s.z.zheng@foxmail.com>
2021-02-05 21:10:31 +0800

    Delete ASV file

--- ExtractMacUserDict.m
+++ ExtractMacUserDict.m
@@ -4,30 +4,38 @@ tic
 
 fidIn = fopen('用户词典.plist', 'r');
 
-plistLineNum = [];   % plist文件中<dict>的行号
+dictTagLineIdx = [];   % plist文件中<dict>的行号
 phraseIdx = 1;    % 短语序号
 lineIdx = 1;
 while ~feof(fidIn)
-    line = fgetl(fidIn);
-    cutLine{lineIdx} = strsplit(line, '\t');   % 按Tab分割行字符串
+    line{lineIdx} = fgetl(fidIn);
+    cutLine{lineIdx} = strsplit(line{lineIdx}, '\t');   % 按Tab分割行字符串
     
     % 寻找<dict>
-    if strcmp(cutLine{lineIdx}(end), '<dict>') == true
+%     if strcmp(cutLine{lineIdx}(end), '<dict>') == true
+    if contains(line{lineIdx}, '<dict>')
 %         disp('Phrase detected.');
-        plistLineNum(phraseIdx) = lineIdx;
+        dictTagLineIdx(phraseIdx) = lineIdx;
         phraseIdx = phraseIdx + 1;       
     end
     
     lineIdx = lineIdx + 1;
 end
 
-[~, numPhrases] = size(plistLineNum);
+[~, numPhrases] = size(dictTagLineIdx);
 phrases = {};
 
 for i = 1: numPhrases
-    phrases{i, 1} = cutLine{plistLineNum(i) + 4}{2}(9: end - 9);     % 拼音
+    
+    strTagStart = zeros(1, 2);
+%     while
+    
+    
+    
+    
+    phrases{i, 1} = cutLine{dictTagLineIdx(i) + 4}{2}(9: end - 9);     % 输入码（拼音）
     phrases{i, 2} = 1;      % 默认出现在第一个候选项
-    phrases{i, 3} = cutLine{plistLineNum(i) + 2}{2}(9: end - 9);     % 短语
+    phrases{i, 3} = cutLine{dictTagLineIdx(i) + 2}{2}(9: end - 9);     % 短语
 end
 
 
